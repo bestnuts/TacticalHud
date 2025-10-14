1aee2b5e053b9b6b91d4c74e3bfc7869b6ab595f
bestnuts <pjh1204c7@gmail.com>
2025-10-14 18:28:06 +0900

    fix: condition null or empty return false

--- src/main/java/me/bestnuts/th/utils/ConditionParser.java
+++ src/main/java/me/bestnuts/th/utils/ConditionParser.java
@@ -8,6 +8,8 @@ import org.mvel2.MVEL;
 public class ConditionParser {
 
     public static boolean parsing(Player player, String condition) {
+        if (condition == null || condition.isEmpty())
+            return false;
         if (TacticalHudPlugin.getInstance().isEnablePlaceHolderAPI())
             condition = PlaceholderAPI.setPlaceholders(player, condition);
         return (Boolean) MVEL.eval(condition);
